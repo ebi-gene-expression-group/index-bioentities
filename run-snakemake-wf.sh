3d37bd34bdeb039c5c8b5cfdc5f92b7a2e6d5654
Pedro Madrigal <pmadrigal@ebi.ac.uk>
2021-10-29 11:09:01 +0100

    WIP snakemake wf

--- run-snakemake-wf.sh
+++ run-snakemake-wf.sh
@@ -1,1 +1,15 @@
+#!/usr/bin/env bash
 
+PROFILE_LINE="--profile profilename"
+SN_CONDA_PREFIX=${SN_CONDA_PREFIX:-$( pwd )/conda_installs}
+CONDA_PREFIX_LINE="--conda-prefix $SN_CONDA_PREFIX"
+USUAL_SM_ERR_OUT=${USUAL_SM_ERR_OUT:-$( pwd )/snakemake.log}
+JOBS=10
+
+snakemake --use-conda --conda-frontend mamba \
+        $PROFILE_LINE \
+        $CONDA_PREFIX_LINE \
+        --latency-wait 10 \
+        --config xxx=$xxx  \
+            yyy=path/to/yyy \
+        --jobs $JOBS -s Snakefile &> $USUAL_SM_ERR_OUT
