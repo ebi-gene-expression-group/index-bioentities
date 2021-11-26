910988b83bdca7952ca2f20c21dd67788487d220
Pablo Moreno <pablo.a.moreno@gmail.com>
2021-11-26 14:27:09 +0000

    Avoids silent failures in deletion of datasets

--- bin/delete_bioentities_species.sh
+++ bin/delete_bioentities_species.sh
@@ -6,10 +6,15 @@ source $scriptDir/common_routines.sh
 require_env_var "SOLR_HOST"
 require_env_var "SPECIES"
 
-curl -X POST -H 'Content-Type: application/json' \
+HTTP_STATUS=$(curl -o >(cat >&3) -w "%{http_code}" -X POST -H 'Content-Type: application/json' \
 "http://$SOLR_HOST/solr/bioentities-v1/update/json?commit=true" --data-binary \
 '{
   "delete": {
     "query": "species:$SPECIES"
   }
-}'
+}')
+
+if [[ ! ${HTTP_STATUS} == 2* ]]
+then
+  echo "Error during delete!" && exit 1
+fi
