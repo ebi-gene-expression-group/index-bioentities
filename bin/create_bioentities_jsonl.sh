b09a51f96239f9f19726bb00579b4ac8718ad91c
Pablo Moreno <pcm32@users.noreply.github.com>
2022-02-11 08:53:23 +0000

    Bioentity maps per accession failure and database pool size (#4)
    * Failed accessions file
    
    * Failed accessions...
    
    * Set min idle connections to reduce initial pool size for more processes
    
    * try minimumIdle instead of minimum-idle
    
    * Move to container version 1.4
    
    * Set pool size and test with 1.5

--- bin/create_bioentities_jsonl.sh
+++ bin/create_bioentities_jsonl.sh
@@ -29,6 +29,7 @@ java_opts="$java_opts -Dexperiment.files.location=$EXPERIMENT_FILES"
 java_opts="$java_opts -Djdbc.url=$jdbc_url"
 java_opts="$java_opts -Djdbc.username=$jdbc_username"
 java_opts="$java_opts -Djdbc.password=$jdbc_password"
+java_opts="$java_opts -Djdbc.max_pool_size=2"
 java_opts="$java_opts -Dserver.port=$server_port"
 
 # This will index everything that it is available in $BIOENTITIES
