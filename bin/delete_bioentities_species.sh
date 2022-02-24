32de4645a1e707478d9adb39adddac3b7b68686f
Pablo Moreno <pcm32@users.noreply.github.com>
2022-02-24 17:33:11 +0000

    Fix bioentities deletion by species (#7)
    * Fix deletion and test
    
    * Missing test file

--- bin/delete_bioentities_species.sh
+++ bin/delete_bioentities_species.sh
@@ -6,13 +6,13 @@ source $scriptDir/common_routines.sh
 require_env_var "SOLR_HOST"
 require_env_var "SPECIES"
 
-HTTP_STATUS=$(curl -o >(cat >&3) -w "%{http_code}" -X POST -H 'Content-Type: application/json' \
-"http://$SOLR_HOST/solr/bioentities-v1/update/json?commit=true" --data-binary \
-'{
-  "delete": {
-    "query": "species:$SPECIES"
-  }
-}')
+# creates a new file descriptor 3 that redirects to 1 (STDOUT)
+exec 3>&1
+
+HTTP_STATUS=$(curl -o >(cat >&3) -w "%{http_code}" -X POST -H "Content-Type: text/xml" \
+"http://$SOLR_HOST/solr/bioentities-v1/update?commit=true" --data-binary \
+"<delete><query>species:$SPECIES</query></delete>"
+)
 
 if [[ ! ${HTTP_STATUS} == 2* ]]
 then
